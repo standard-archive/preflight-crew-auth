4d0e4cff47b3a313967182e79ca306b143b8c8ca
lichumestandard-sudo <lichumestandard@gmail.com>
2026-09-23 05:06:10 +0200

    Day 21: fix widget test after login/signup redesign changed label text

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-import 'package:preflight_crew_auth/src/screens/signup_screen.dart';
+import "package:flutter/material.dart";
+import "package:flutter_test/flutter_test.dart";
+import "package:preflight_crew_auth/src/screens/signup_screen.dart";
 
 void main() {
   testWidgets('Signup screen displays the student account form',
@@ -13,9 +13,8 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.text('Join PreFlight Crew'), findsOneWidget);
-    expect(find.text('Create Account'), findsOneWidget);
     expect(find.text('Sign Up'), findsOneWidget);
     expect(find.text('WeThinkCode Student Email'), findsOneWidget);
-    expect(find.text('Password'), findsOneWidget);
+    expect(find.text('Password (min 6 characters)'), findsOneWidget);
   });
 }
